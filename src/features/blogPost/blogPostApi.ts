31ef8d8f884fd3670ef4977567af64b205bde16b
Pavel Antropov <Pavel_Antropov@gmail.com>
2022-03-11 16:53:27 +0400

    Create an API call

--- src/features/blogPost/blogPostApi.ts
+++ src/features/blogPost/blogPostApi.ts
@@ -36,5 +36,20 @@ export function fetchBlogPosts(): Promise<FetchBlogPostsResponse> {
     });
   } else {
     throw new Error("Not implemented");
+}
+
+export async function createBlogPost(
+  params: CreateBlogPostRequest
+): Promise<CreateBlogPostResponse> {
+  if (process.env.NODE_ENV === "test" || true) {
+    return new Promise<CreateBlogPostResponse>((resolve) => {
+      resolve({} as CreateBlogPostResponse);
+    });
+  } else {
+    return await fetch(`${process.env.REACT_APP_API_URL}/api/blogPosts`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(params),
+    });
   }
 }
