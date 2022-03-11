f60e47987d966fc3c888bf46789cd0bb9f784e6b
Pavel Antropov <Pavel_Antropov@gmail.com>
2022-03-11 14:16:24 +0400

    Update API mock

--- src/features/blogPost/blogPostApi.ts
+++ src/features/blogPost/blogPostApi.ts
@@ -6,14 +6,15 @@ export function fetchBlogPost(
 ): Promise<FetchBlogPostResponse> {
   if (process.env.NODE_ENV === "test" || true) {
     return new Promise<FetchBlogPostResponse>((resolve) => {
-      const blogPost = {
-        title: "Test blog post",
-        body: "My name is Pavel Antropov and I'm the author of this blog. I hope that the support of the blog will help me to improve my knowledge and understanding of the various important topics connected with full-stack .NET development, as writing these posts I'm going to repeat and research the corresponding information.",
-        creationDate: new Date(),
-      };
+      let blogPost;
+
+      DefaultBlogPosts.forEach(post => {
+        if (post.blogPostId === blogPostId)
+          blogPost = post;
+      });
 
       resolve({
-        blogPost,
+        blogPost: blogPost ?? DefaultBlogPosts[0],
       } as FetchBlogPostResponse);
     });
   } else {
