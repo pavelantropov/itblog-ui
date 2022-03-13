f276394aeb3243fe0f9a4b99103dee6b665b05ca
Pavel Antropov <p.m.antropov@gmail.com>
2022-03-13 17:47:34 +0400

    Fix tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,15 +1,17 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import { Provider } from 'react-redux';
-import { store } from './app/store';
-import App from './App';
+import { render } from "@testing-library/react";
+import { Provider } from "react-redux";
+import { store } from "./app/store";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
 
-test('renders learn react link', () => {
+test("renders blog title", () => {
   const { getByText } = render(
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <BrowserRouter>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </BrowserRouter>
   );
 
-  expect(getByText(/learn/i)).toBeInTheDocument();
+  expect(getByText(/antropov it blog/i)).toBeInTheDocument();
 });
