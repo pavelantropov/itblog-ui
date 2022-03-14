1b193dc22dd10bc4a24477f1d66d00cbb26a6208
Pavel Antropov <p.m.antropov@gmail.com>
2022-03-15 00:41:12 +0400

    Update test data

--- src/features/blogPost/testData.ts
+++ src/features/blogPost/testData.ts
@@ -6,11 +6,16 @@ export const DefaultBlogPosts: BlogPost[] = [
     title: "Test blog post",
     body: "My name is Pavel Antropov and I'm the author of this blog. I hope that the support of the blog will help me to improve my knowledge and understanding of the various important topics connected with full-stack .NET development, as writing these posts I'm going to repeat and research the corresponding information.",
     creationDate: new Date(),
+    lastUpdateDate: new Date(),
+    tags: ["offtopic", "bio"],
+    author: "pavelantropov",
   },
   {
     id: "2",
     title: "Hello world",
     body: "Hello world!",
     creationDate: new Date(),
+    tags: ["test"],
+    author: "pavelantropov",
   },
 ];
