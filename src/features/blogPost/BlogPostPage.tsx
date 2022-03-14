23af807e5b419b6ed4cb67ab35e15aeecbb06c2a
Pavel Antropov <p.m.antropov@gmail.com>
2022-03-15 00:42:34 +0400

    Update the BlogPostPage component

--- src/features/blogPost/BlogPostPage.tsx
+++ src/features/blogPost/BlogPostPage.tsx
@@ -1,3 +1,4 @@
+import Chip from "@mui/material/Chip";
 import Typography from "@mui/material/Typography";
 import * as React from "react";
 import { useParams } from "react-router-dom";
@@ -14,18 +15,34 @@ export default function BlogPostPage() {
 
   return (
     <>
-      <Typography variant="h1" component="div" gutterBottom>
-        {blogPost?.title ?? "***"}
-      </Typography>
-      <Typography variant="subtitle1" gutterBottom component="div">
+      <Typography variant="h1">{blogPost?.title ?? "***"}</Typography>
+      <Typography variant="body2">
         Posted:{" "}
         {blogPost?.creationDate
           ? blogPost.creationDate.toLocaleString()
           : "unknown"}
       </Typography>
-      <Typography variant="body1" gutterBottom>
+      <Typography variant="body2" mb={2}>
+        Last update:{" "}
+        {blogPost?.lastUpdateDate
+          ? blogPost.lastUpdateDate.toLocaleString()
+          : "unknown"}
+      </Typography>
+      <Typography variant="body1" mb={1.5}>
         {blogPost?.body ?? "-"}
       </Typography>
+      <Typography variant="body1" mb={1.5}>
+        @{blogPost?.author ?? "Anonimous"}
+      </Typography>
+      {blogPost?.tags &&
+        blogPost.tags.length > 0 &&
+        blogPost.tags.map((tag, index) => (
+          <Chip 
+            key={`blogPost_${index}`} 
+            label={tag} 
+            sx={{ mr: 0.5 }} 
+          />
+        ))}
     </>
   );
 }
