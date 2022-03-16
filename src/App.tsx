01360dcc3a80d0231ed45bf0c10d8be96700ce0d
Pavel Antropov <p.m.antropov@gmail.com>
2022-03-17 00:34:38 +0400

    Add dark theme

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,28 @@ import Layout from "./features/layout/Layout";
 import { Routes, Route } from "react-router-dom";
 import CreateBlogPost from "./features/blogPost/CreateBlogPost";
 import BlogPostsList from "./features/blogPost/BlogPostsList";
+import { ThemeProvider, createTheme, useMediaQuery } from "@mui/material";
 
 function App() {
+  const prefersDarkMode = useMediaQuery("(prefers-color-scheme: dark)");
+
+  const theme = createTheme({
+    palette: {
+      mode: prefersDarkMode ? "dark" : "light",
+    },
+  });
+
   return (
     <>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<BlogPostsList />} />
-          <Route path=":blogPostId" element={<BlogPostPage />} />
-          <Route path="create" element={<CreateBlogPost />} />
-        </Route>
-      </Routes>
+      <ThemeProvider theme={theme}>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<BlogPostsList />} />
+            <Route path=":blogPostId" element={<BlogPostPage />} />
+            <Route path="create" element={<CreateBlogPost />} />
+          </Route>
+        </Routes>
+      </ThemeProvider>
     </>
   );
 }
--- src/features/layout/Layout.tsx
+++ src/features/layout/Layout.tsx
@@ -5,9 +5,20 @@ import TopBar from "./TopBar";
 export default function Layout() {
   return (
     <>
-      <TopBar />
-      <Box sx={{ width: "100%", maxWidth: 1000, ml: "3vw", mt: "30px" }}>
-        <Outlet />
+      <Box
+        sx={{
+          bgcolor: "background.default",
+          color: "text.primary",
+          width: "100%",
+          height: "100%",
+          minWidth: "100vw",
+          minHeight: "100vh",
+        }}
+      >
+        <TopBar />
+        <Box sx={{ width: "100%", maxWidth: 1000, ml: "3vw", mt: "30px" }}>
+          <Outlet />
+        </Box>
       </Box>
     </>
   );
